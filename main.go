39939907bb73a76a51d71f1648881a498a51ba62
rjain032 <rajat.jain@email.ucr.edu>
2022-08-12 15:38:04 -0700

    CLI app

--- main.go
+++ main.go
@@ -3,5 +3,31 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("Hello World!")
+
+	var appName  = "GoBookingApp"
+	const totalNumberOfTickets = 50
+	var remainingNumberOfTickets = 50
+
+	fmt.Printf("Welcome to %s.\n",appName)
+	fmt.Println("Use this app to book tickets.")
+	fmt.Printf("%v tickets available out of %v.\n",remainingNumberOfTickets,totalNumberOfTickets)
+
+	var firstName string
+	var lastName string
+	var email string
+	var numberOfTicketsBooked int
+	fmt.Println("Enter your first name")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your last name")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter your email")
+	fmt.Scan(&email)
+	fmt.Println("Enter number of tickets you want to book")
+	fmt.Scan(&numberOfTicketsBooked)
+	remainingNumberOfTickets -= numberOfTicketsBooked
+	fmt.Printf("\n\n%v %v booked %v tickets.\nYou will receive a confirmation email at %v.\nThank You\n",firstName,lastName,numberOfTicketsBooked,email)
+	fmt.Printf("%v tickets left.\n",remainingNumberOfTickets)
+	
+
+
 }
